e9f58ab6fea878b36a287fc41339afbf61cf0d46
Wil-dev <mehdididou7@icloud.com>
2025-01-06 23:29:10 +0100

    modification du fichier context

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,13 +1,54 @@
 import React from "react";
+import * as authNetflix from "../utils/authNetflixProvider";
+import { clientAuth } from "../utils/clientApi";
+import { useFetchData } from "../utils/hooks";
+import { useQueryClient } from "react-query";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const AuthContext = React.createContext();
 
 const useAuth = () => {
-  const context = React.useContext(AuthContext)
-  if (!context) {
-    throw new Error("useAuth() s'utilise avec <AuthContext.provider>")
-  }
-  return context
-}
+	const context = React.useContext(AuthContext);
+	if (!context) {
+		throw new Error("useAuth() s'utilise avec <AuthContext.provider>");
+	}
+	return context;
+};
+// async function getUserByToken() {
+// 	let user = null;
+// 	const token = await authNetflix.getToken();
+// 	if (token) {
+// 		const data = await clientAuth("me", { token });
+// 		user = data.data.user;
+// 	}
+// 	return user;
+// }
 
-export {AuthContext, useAuth};
+// const AuthProvider = (props) => {
+// 	const queryClient = useQueryClient();
+// 	const { data: authUser, execute, status, setData } = useFetchData();
+// 	React.useEffect(() => {
+// 		execute(getUserByToken());
+// 	}, [execute]);
+
+// 	const [authError, setAuthError] = React.useState();
+// 	const login = (data) =>
+// 		authNetflix
+// 			.login(data)
+// 			.then((user) => setData(user))
+// 			.catch((err) => setAuthError(err));
+// 	const register = (data) =>
+// 		authNetflix
+// 			.register(data)
+// 			.then((user) => setData(user))
+// 			.catch((err) => setAuthError(err));
+// 	const logout = () => {
+// 		authNetflix.logout();
+// 		queryClient.clear();
+// 		setData(null);
+// 	};
+	
+// };
+
+export { AuthContext, useAuth };
